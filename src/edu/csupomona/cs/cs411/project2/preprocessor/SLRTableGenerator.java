514f1764719f9b5939bfe55b3eaca626f8de1b76
Collin Smith <collinsmith@csupomona.edu>
2014-03-01 02:12:18 -0800

    Deleted old output file and changed a couple of names in the TableGenerator class

--- src/edu/csupomona/cs/cs411/project2/preprocessor/SLRTableGenerator.java
+++ src/edu/csupomona/cs/cs411/project2/preprocessor/SLRTableGenerator.java
@@ -21,24 +21,24 @@ public class SLRTableGenerator {
 	private static final String NONTERMINAL_DELIMINATOR = "[A-Z][a-zA-Z0-9]*:";
 
 	private int numNonterminals = 0;
-	private Map<String, Integer> symbols;
-	private Map<Integer, List<List<Integer>>> productions;
+	private Map<String, Integer> symbolTable;
+	private Map<Integer, List<List<Integer>>> productionTable;
 
 	public SLRTableGenerator(Path p) throws IOException {
-		productions = new HashMap<>();
-		symbols = new LinkedHashMap<>();
+		productionTable = new HashMap<>();
+		symbolTable = new LinkedHashMap<>();
 
 		generateNonterminals(p);
 		for (Keywords k : Keywords.values()) {
-			symbols.put(k.name(), numNonterminals+k.ordinal()+1);
+			symbolTable.put(k.name(), numNonterminals+k.ordinal()+1);
 			if (Keywords.ACTUAL_KEYWORDS.contains(k) || Keywords.OPERATORS.contains(k)) {
-				symbols.put(k.getRegex(), numNonterminals+k.ordinal()+1);
+				symbolTable.put(k.getRegex(), numNonterminals+k.ordinal()+1);
 			}
 		}
 
 		// TODO I know I'm not stupid enough to do it, but productions should be immutable
 		generateProductions(p);
-		for (Entry<Integer, List<List<Integer>>> productionList : productions.entrySet()) {
+		for (Entry<Integer, List<List<Integer>>> productionList : productionTable.entrySet()) {
 			for (List<Integer> production : productionList.getValue()) {
 				production = Collections.unmodifiableList(production);
 			}
@@ -46,8 +46,8 @@ public class SLRTableGenerator {
 			productionList.setValue(Collections.unmodifiableList(productionList.getValue()));
 		}
 
-		symbols = Collections.unmodifiableMap(symbols);
-		productions = Collections.unmodifiableMap(productions);
+		symbolTable = Collections.unmodifiableMap(symbolTable);
+		productionTable = Collections.unmodifiableMap(productionTable);
 	}
 
 	private void generateNonterminals(Path p) throws IOException {
@@ -56,8 +56,8 @@ public class SLRTableGenerator {
 			String line;
 			while ((line = file.readLine()) != null) {
 				if (line.matches(NONTERMINAL_DELIMINATOR)) {
-					symbols.put(line.substring(0, line.length()-1), ++numNonterminals);
-					productions.put(numNonterminals, new LinkedList<List<Integer>>());
+					symbolTable.put(line.substring(0, line.length()-1), ++numNonterminals);
+					productionTable.put(numNonterminals, new LinkedList<List<Integer>>());
 				}
 			}
 		}
@@ -77,13 +77,13 @@ public class SLRTableGenerator {
 				}
 
 				if (line.matches(NONTERMINAL_DELIMINATOR)) {
-					currentProduction = symbols.get(line.substring(0, line.length()-1));
+					currentProduction = symbolTable.get(line.substring(0, line.length()-1));
 					assert currentProduction <= numNonterminals;
 					continue;
 				}
 
 				production = new LinkedList<>();
-				productionRules = productions.get(currentProduction);
+				productionRules = productionTable.get(currentProduction);
 				productionRules.add(production);
 
 				line = line.trim();
@@ -106,23 +106,32 @@ public class SLRTableGenerator {
 	}
 
 	private int resolveIndex(String token) {
-		if (symbols.containsKey(token)) {
-			return symbols.get(token);
+		if (symbolTable.containsKey(token)) {
+			return symbolTable.get(token);
 		}
 
 		return -1;
 	}
 
+	public boolean isNonterminal(String s) {
+		Integer id = symbolTable.get(s);
+		if (id == null) {
+			return false;
+		}
+
+		return id <= numNonterminals;
+	}
+
 	public void dump() throws IOException {
 		Charset charset = Charset.forName("US-ASCII");
 		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(".", "output", "toy.sym.dump.txt"), charset, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
-			for (Entry<String, Integer> entry : symbols.entrySet()) {
+			for (Entry<String, Integer> entry : symbolTable.entrySet()) {
 				writer.write(String.format("%3d %s%n", entry.getValue(), entry.getKey()));
 			}
 		}
 
 		Map<Integer, String> reverseEngineeredSymbols = new HashMap<>();
-		for (Entry<String, Integer> entry : symbols.entrySet()) {
+		for (Entry<String, Integer> entry : symbolTable.entrySet()) {
 			if (entry.getKey().charAt(0) != '_' && !Character.isUpperCase(entry.getKey().charAt(0))) {
 				continue;
 			}
@@ -131,7 +140,7 @@ public class SLRTableGenerator {
 		}
 
 		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(".", "output", "toy.cfg.dump.txt"), charset, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)) {
-			for (Entry<Integer, List<List<Integer>>> entry : productions.entrySet()) {
+			for (Entry<Integer, List<List<Integer>>> entry : productionTable.entrySet()) {
 				writer.write(String.format("%s:%n", reverseEngineeredSymbols.get(entry.getKey())));
 				for (List<Integer> l : entry.getValue()) {
 					writer.write(String.format("\t"));
